905b662b665343ccc85de7da2c28315d637d0036
여인영 <marineyeong@gmail.com>
2024-07-25 14:40:56 +0900

    [Feat] : useMemo를 활용하여 현재 상태에 따른 Todo 갯수 추적 기능 추가

--- TodoList/src/components/List.jsx
+++ TodoList/src/components/List.jsx
@@ -1,6 +1,6 @@
 import './List.css';
 import TodoItem from './TodoItem';
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 
 const List = ({ todos, onUpdate, onDelete }) => {
   const [search, setSearch] = useState('');
@@ -17,9 +17,27 @@ const List = ({ todos, onUpdate, onDelete }) => {
   };
 
   const filteredTodos = getFilteredData();
+
+  const { totalCount, doneCount, notDoneCount } = useMemo(() => {
+    // 현재 Todo들의 상태를 분석해서 수치로 제공하는 함수
+
+    const totalCount = todos.length; //전체 Todo item 갯수
+    const doneCount = todos.filter((todo) => todo.isDone).length; // 전체 Todo 중 완료된 것
+    const notDoneCount = totalCount - doneCount; // 완료 되지 않은 Todo
+
+    return {
+      totalCount,
+      doneCount,
+      notDoneCount,
+    };
+  }, [todos]);
+
   return (
     <div className="List">
       <h4>Todo List✅</h4>
+      <div>total: {totalCount}</div>
+      <div>done: {doneCount}</div>
+      <div>notDone: {notDoneCount}</div>
       <input value={search} onChange={onChangeSearch} placeholder="검색어를 입력하세요" />
       <div className="todos_wrapper">
         {filteredTodos.map((todo) => {
