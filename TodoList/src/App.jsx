de764b13014528feddac942eff591bd3bf1969a7
여인영 <marineyeong@gmail.com>
2024-07-25 13:50:59 +0900

    Refactor: useReducer를 사용하여 상태 업데이트 로직 분리

--- TodoList/src/App.jsx
+++ TodoList/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState, useRef } from 'react';
+import { useState, useRef, useReducer } from 'react';
 import Header from './components/Header';
 import Editor from './components/Editor';
 import List from './components/List';
@@ -25,26 +25,41 @@ const mockData = [
   },
 ];
 
+function reducer(state, action) {
+  switch (action.type) {
+    case 'CREATE':
+      return [action.data, ...state]; //반환한 새로운 State 값
+    case 'UPDATE':
+      return state.map((item) => (item.id === action.targetId ? { ...item, isDone: !item.isDone } : item));
+    case 'DELETE':
+      return state.filter((item) => item.id !== action.targetId);
+    default:
+      return state;
+  }
+}
+
 function App() {
-  const [todos, setTodos] = useState(mockData);
+  const [todos, dispatch] = useReducer(reducer, mockData);
   const idRef = useRef(4);
 
   const onCreate = (content) => {
-    const newTodo = {
-      id: idRef.current++,
-      isDone: false,
-      content: content,
-      date: new Date().getTime(),
-    };
-    setTodos([newTodo, ...todos]);
+    dispatch({
+      type: 'CREATE',
+      data: {
+        id: idRef.current++,
+        isDone: false,
+        content: content,
+        date: new Date().getTime(),
+      },
+    });
   };
 
   const onUpdate = (targetId) => {
-    setTodos(todos.map((todo) => (todo.id === targetId ? { ...todo, isDone: !todo.isDone } : todo)));
+    dispatch({ type: 'UPDATE', targetId: targetId });
   };
 
   const onDelete = (targetId) => {
-    setTodos(todos.filter((todo) => todo.id !== targetId));
+    dispatch({ type: 'DELETE', targetId: targetId });
   };
   return (
     <div className="App">
